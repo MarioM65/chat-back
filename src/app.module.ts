30bfbde4f64288a66b0085cd092a1a5ae56fbd9c
MarioM65 <mmystirio7@gmail.com>
2025-11-12 09:22:10 +0100

    pull

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,12 @@ import { UserService } from './services/user/user.service';
 import { UserBloqueadoService } from './services/user_bloqueado/user_bloqueado.service';
 import { UserModule } from './modules/user/user.module';
 import { UserBloqueadoModule } from './modules/user_bloqueado/user_bloqueado.module';
+import { ConversaService } from './services/conversa/conversa.service';
+import { ParticipanteConversaService } from './services/participante_conversa/participante_conversa.service';
 
 @Module({
   imports: [],
-  controllers: [ UsersController, UsersBloqueadosController, AuthController],
-  providers: [PrismaService, UserService, UserBloqueadoService],
+  controllers: [ UsersController, UsersBloqueadosController, AuthController,ParticipanteConversaController, ConversaController],
+  providers: [PrismaService, UserService, UserBloqueadoService, ConversaService, ParticipanteConversaService],
 })
 export class AppModule {}
