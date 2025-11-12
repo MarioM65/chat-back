9f504d0fe801f3ea7a8449c21939c049acdc6ce7
Suelylacerda <147233413+Suelylacerda@users.noreply.github.com>
2025-11-12 08:35:24 +0100

    Criação das tabelas mensagem e anexo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { AuthController } from './controllers/auth/auth.controller';
 import { PrismaService } from './services/prisma/prisma.service';
 import { UserService } from './services/user/user.service';
 import { UserBloqueadoService } from './services/user_bloqueado/user_bloqueado.service';
+import { MensagensController } from './controllers/mensagens/mensagens.controller';
+import { MensagensService } from './services/mensagens/mensagens.service';
+import { AnexosController } from './controllers/anexos/anexos.controller';
+import { AnexosService } from './services/anexos/anexos.service';
 
 @Module({
   imports: [],
-  controllers: [ UsersController, UsersBloqueadosController, AuthController],
-  providers: [PrismaService, UserService, UserBloqueadoService],
+  controllers: [ UsersController, UsersBloqueadosController, AuthController, MensagensController, AnexosController],
+  providers: [PrismaService, UserService, UserBloqueadoService, MensagensService, AnexosService],
 })
 export class AppModule {}
---
+++ src/controllers/mensagens/mensagens.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('mensagens')
+export class MensagensController {}
