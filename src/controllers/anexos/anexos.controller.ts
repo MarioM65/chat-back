7d57a214a12f379251446e1cd15f79e3645283e4
Suelylacerda <147233413+Suelylacerda@users.noreply.github.com>
2025-11-12 11:10:30 +0100

    Tabelas mensagem e anexo

--- src/controllers/anexos/anexos.controller.ts
+++ src/controllers/anexos/anexos.controller.ts
@@ -1,4 +1,77 @@
-import { Controller } from '@nestjs/common';
+import {  Body, Controller, Delete, Get,Param,Post,Put,UploadedFile,UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Anexo } from 'generated/prisma';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { CreateAnexo } from 'src/interfaces/anexos';
+import { AnexosService } from 'src/services/anexos/anexos.service';
 
 @Controller('anexos')
-export class AnexosController {}
+export class AnexosController {
+  constructor(private readonly anexosService: AnexosService) {}
+
+  @Get()
+  async getAnexos(): Promise<Anexo[]> {
+    return this.anexosService.getAllAnexos();
+  }
+
+  @Get(':id')
+  async getAnexoById(@Param('id') id: number): Promise<Anexo | null> {
+    return this.anexosService.getAnexoById(Number(id));
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('arquivo', {
+      storage: diskStorage({
+        destination: './uploads/anexos',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileExt = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${fileExt}`);
+        },
+      }),
+    }),
+  )
+  async createAnexo(
+    @Body() data: CreateAnexo,
+    @UploadedFile() arquivo?: Express.Multer.File,
+  ): Promise<Anexo> {
+    if (arquivo) {
+      data.caminho_arquivo = join('uploads/anexos', arquivo.filename);
+      data.nome_arquivo = arquivo.originalname;
+    }
+    return this.anexosService.createAnexo(data);
+  }
+
+  @Put(':id')
+  @UseInterceptors(
+    FileInterceptor('arquivo', {
+      storage: diskStorage({
+        destination: './uploads/anexos',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileExt = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${fileExt}`);
+        },
+      }),
+    }),
+  )
+  async updateAnexo(
+    @Param('id') id: number,
+    @Body() data: Partial<CreateAnexo>,
+    @UploadedFile() arquivo?: Express.Multer.File,
+  ): Promise<Anexo> {
+    if (arquivo) {
+      data.caminho_arquivo = join('uploads/anexos', arquivo.filename);
+      data.nome_arquivo = arquivo.originalname;
+    }
+    return this.anexosService.updateAnexo(Number(id), data);
+  }
+
+  @Delete(':id')
+  async deleteAnexo(@Param('id') id: number): Promise<Anexo> {
+    return this.anexosService.deleteAnexo(Number(id));
+  }
+}
--- src/controllers/mensagens/mensagens.controller.ts
+++ src/controllers/mensagens/mensagens.controller.ts
@@ -1,4 +1,75 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Mensagem } from 'generated/prisma';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { CreateMensagem } from 'src/interfaces/mensagens';
+import { MensagensService } from 'src/services/mensagens/mensagens.service';
 
 @Controller('mensagens')
-export class MensagensController {}
+export class MensagensController {
+  constructor(private readonly mensagensService: MensagensService) {}
+
+  @Get()
+  async getMensagens(): Promise<Mensagem[]> {
+    return this.mensagensService.getAllMensagens();
+  }
+
+  @Get(':id')
+  async getMensagemById(@Param('id') id: number): Promise<Mensagem | null> {
+    return this.mensagensService.getMensagemById(Number(id));
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('arquivo', {
+      storage: diskStorage({
+        destination: './uploads/mensagens_anexos',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileExt = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${fileExt}`);
+        },
+      }),
+    }),
+  )
+  async createMensagem(
+    @Body() data: CreateMensagem,
+    @UploadedFile() arquivo?: Express.Multer.File,
+  ): Promise<Mensagem> {
+    if (arquivo) {
+      data.tipo = join('uploads/mensagens_anexos', arquivo.filename);
+    }
+    return this.mensagensService.createMensagem(data);
+  }
+
+  @Put(':id')
+  @UseInterceptors(
+    FileInterceptor('arquivo', {
+      storage: diskStorage({
+        destination: './uploads/mensagens_anexos',
+        filename: (req, file, cb) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileExt = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${fileExt}`);
+        },
+      }),
+    }),
+  )
+  async updateMensagem(
+    @Param('id') id: number,
+    @Body() data: Partial<CreateMensagem>,
+    @UploadedFile() arquivo?: Express.Multer.File,
+  ): Promise<Mensagem> {
+    if (arquivo) {
+      data.tipo = join('uploads/mensagens_anexos', arquivo.filename);
+    }
+    return this.mensagensService.updateMensagem(Number(id), data);
+  }
+
+  @Delete(':id')
+  async deleteMensagem(@Param('id') id: number): Promise<Mensagem> {
+    return this.mensagensService.deleteMensagem(Number(id));
+  }
+}
